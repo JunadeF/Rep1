a96dcc9e907e78a45def1ca5461c20e9f8b06b9f
Junadef <junade.frizlar@gmail.com>
2020-03-01 22:22:56 +0200

    New elective 3 assignment 1

---
+++ app/src/main/java/com/junadefrizlar/Display.java
@@ -0,0 +1,12 @@
+package com.junadefrizlar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Display extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.display);
+    }
+}
